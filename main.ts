e1df5fa9f639ad1722faa0eb30b31b93e7852d41
eduard.17072006@gmail.com <eduard.17072006@gmail.com>
2022-05-27 23:05:13 +0300

    ts

--- main.ts
+++ main.ts
@@ -47,7 +47,7 @@ function win<T>(arg: T) : void {
 
 */
 
-/*
+
 function getArray<T>(items : T[] ) : T[] {
     return new Array<T>().concat(items);
 }
@@ -58,5 +58,5 @@ let myStrArr = getArray<string>(["Hello", "World"]);
 
 myNumArr.push(400); // OK
 myStrArr.push("Hello TypeScript"); // OK
-*/
+
 
